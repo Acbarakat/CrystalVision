6577be6ceba224ea946a2386802afa64dec4460e
Allonte <angelosheltie@gmail.com>
2023-04-01 22:08:49 -0700

    Update gatherdata.py
    Update todo list

--- src/gatherdata.py
+++ src/gatherdata.py
@@ -8,9 +8,9 @@ Attributes:
 
 Todo:
     * Archive all image data
-    * Fill data with missing Opus XVII Starter cards
     * Fill data with missing Anniversary Only cards
     * Mist Dragon (Full Art) is missing
+    * Add Illustrator data (http://www.square-enix-shop.com/jp/ff-tcg/card/illust_index.html)
 
 """
 import asyncio
