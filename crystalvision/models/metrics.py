391cd640bb6e42c2f734d4ad2de0e63e0c2923b2
Allonte <angelosheltie@gmail.com>
2024-02-02 14:05:11 -0800

    Custom OneHot metric accuracy

--- crystalvision/models/metrics.py
+++ crystalvision/models/metrics.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Optional, Union, List, Tuple
 
 import tensorflow.compat.v2 as tf
 from keras import metrics
@@ -57,10 +57,129 @@ class MyOneHotMeanIoU(metrics.OneHotMeanIoU):
         #     y_true = tf.argmax(y_true, axis=self.axis)
         # if not self.sparse_y_pred:
         #     y_pred = tf.argmax(y_pred, axis=self.axis)
-        y_pred = tf.where(y_pred >= self.threshold, 1.0, 0.0)
+        y_pred = tf.where(y_pred >= self.threshold, 1.0, y_pred)
 
         y_true = tf.cast(y_true, self._dtype)
         y_pred = tf.cast(y_pred, self._dtype)
+
+        # Flatten the input if its rank > 1.
+        if y_pred.shape.ndims > 1:
+            y_pred = tf.reshape(y_pred, [-1])
+
+        if y_true.shape.ndims > 1:
+            y_true = tf.reshape(y_true, [-1])
+
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, self._dtype)
+            if sample_weight.shape.ndims > 1:
+                sample_weight = tf.reshape(sample_weight, [-1])
+
+        if self.ignore_class is not None:
+            ignore_class = tf.cast(self.ignore_class, y_true.dtype)
+            valid_mask = tf.not_equal(y_true, ignore_class)
+            y_true = y_true[valid_mask]
+            y_pred = y_pred[valid_mask]
+            if sample_weight is not None:
+                sample_weight = sample_weight[valid_mask]
+
+        # Accumulate the prediction to current confusion matrix.
+        current_cm = tf.math.confusion_matrix(
+            y_true,
+            y_pred,
+            self.num_classes,
+            weights=sample_weight,
+            dtype=self._dtype,
+        )
+        return self.total_cm.assign_add(current_cm)
+
+
+class MyOneHotIoU(metrics._IoUBase):
+    @dtensor_utils.inject_mesh
+    def __init__(
+        self,
+        target_class_ids: Union[List[int], Tuple[int, ...]],
+        threshold: int | float,
+        name=None,
+        dtype=None,
+        ignore_class: Optional[int] = None,
+        sparse_y_pred: bool = True,
+        axis: int = -1,
+    ):
+        super().__init__(
+            num_classes=len(target_class_ids),
+            name=name,
+            dtype=dtype,
+            ignore_class=ignore_class,
+            sparse_y_pred=sparse_y_pred,
+            axis=axis,
+        )
+        self.threshold = threshold
+        self.target_class_ids = target_class_ids
+
+    def result(self):
+        """Compute the intersection-over-union via the confusion matrix."""
+        sum_over_row = tf.cast(tf.reduce_sum(self.total_cm, axis=0), dtype=self._dtype)
+        sum_over_col = tf.cast(tf.reduce_sum(self.total_cm, axis=1), dtype=self._dtype)
+        true_positives = tf.cast(
+            tf.linalg.tensor_diag_part(self.total_cm), dtype=self._dtype
+        )
+
+        # sum_over_row + sum_over_col =
+        #     2 * true_positives + false_positives + false_negatives.
+        denominator = sum_over_row + sum_over_col - true_positives
+
+        # Only keep the target classes
+        # true_positives = tf.gather(true_positives, self.target_class_ids)
+        # denominator = tf.gather(denominator, self.target_class_ids)
+
+        # If the denominator is 0, we need to ignore the class.
+        num_valid_entries = tf.reduce_sum(
+            tf.cast(tf.not_equal(denominator, 0), dtype=self._dtype)
+        )
+
+        iou = tf.math.divide_no_nan(true_positives, denominator)
+
+        return tf.math.divide_no_nan(
+            tf.reduce_sum(iou, name="mean_iou"), num_valid_entries
+        )
+
+    def get_config(self):
+        return {
+            "num_classes": self.num_classes,
+            "target_class_ids": self.target_class_ids,
+            "threshold": self.threshold,
+            "name": self.name,
+            "dtype": self._dtype,
+            "ignore_class": self.ignore_class,
+            "sparse_y_pred": self.sparse_y_pred,
+            "axis": self.axis,
+        }
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """Accumulates the confusion matrix statistics.
+
+        Args:
+          y_true: The ground truth values.
+          y_pred: The predicted values.
+          sample_weight: Optional weighting of each example. Defaults to 1. Can
+            be a `Tensor` whose rank is either 0, or the same rank as `y_true`,
+            and must be broadcastable to `y_true`.
+
+        Returns:
+          Update op.
+        """
+
+        # if not self.sparse_y_true:
+        #     y_true = tf.argmax(y_true, axis=self.axis)
+        # if not self.sparse_y_pred:
+        #     y_pred = tf.argmax(y_pred, axis=self.axis)
+        y_pred = tf.where(y_pred >= self.threshold, 1.0, y_pred)
+
+        y_true = tf.cast(y_true, self._dtype)
+        y_pred = tf.cast(y_pred, self._dtype)
+
+        y_true = tf.gather(y_true, self.target_class_ids, axis=1)
+        y_pred = tf.gather(y_pred, self.target_class_ids, axis=1)
 
         # Flatten the input if its rank > 1.
         if y_pred.shape.ndims > 1:
--- crystalvision/models/multioutput.py
+++ crystalvision/models/multioutput.py
@@ -9,23 +9,25 @@ Todo:
 from typing import List
 
 from pandas import DataFrame
-from keras import layers, models, callbacks
+from keras import layers, models, callbacks, metrics
 from keras_tuner import HyperParameters
 from sklearn.preprocessing import MultiLabelBinarizer
 
 try:
     from . import CardModel
+    from .metrics import MyOneHotMeanIoU, MyOneHotIoU
     from .callbacks import StopOnValue
-    from .mixins.compiles import BinaryCrossMixin
+    from .mixins.compiles import OneHotMeanIoUMixin
     from .mixins.tuners import BayesianOptimizationTunerMixin
 except ImportError:
     from __init__ import CardModel
+    from crystalvision.models.metrics import MyOneHotMeanIoU, MyOneHotIoU
     from crystalvision.models.callbacks import StopOnValue
-    from crystalvision.models.mixins.compiles import BinaryCrossMixin
+    from crystalvision.models.mixins.compiles import OneHotMeanIoUMixin
     from crystalvision.models.mixins.tuners import BayesianOptimizationTunerMixin
 
 
-class MultiLabel(BinaryCrossMixin, BayesianOptimizationTunerMixin, CardModel):
+class MultiLabel(OneHotMeanIoUMixin, BayesianOptimizationTunerMixin, CardModel):
     MAX_TRIALS = 50
     DEFAULT_EPOCHS = 50
 
@@ -39,12 +41,26 @@ class MultiLabel(BinaryCrossMixin, BayesianOptimizationTunerMixin, CardModel):
         self.df: DataFrame = self.filter_dataframe(df.copy())
         self.vdf: DataFrame = self.filter_dataframe(vdf.copy())
 
-        self.feature_key: str = ["type_en", "cost", "element"]
+        self.feature_key: List[str] = ["type_en", "cost", "element"]
         self.stratify_cols: List[str] = ["type_en", "cost", "element"]
 
-        self.labels = []
+        self.labels: List[str] = []
         for fkey in self.feature_key:
             self.labels.extend(self.df[fkey].unique())
+
+        idx: int = 0
+        self._metrics: List[metrics.Metric] = []
+        for fkey in self.feature_key:
+            labels = self.df[fkey].unique()
+            self._metrics.append(
+                MyOneHotIoU(
+                    target_class_ids=list(range(idx, len(labels) + idx)),
+                    threshold=0.95,
+                    name=f"{fkey}_accuracy",
+                ),
+                # ignore_class
+            )
+            idx += len(labels)
 
         self.mlb = MultiLabelBinarizer(classes=self.labels)
 
@@ -57,9 +73,9 @@ class MultiLabel(BinaryCrossMixin, BayesianOptimizationTunerMixin, CardModel):
 
         self.callbacks = [
             callbacks.EarlyStopping(
-                monitor="val_accuracy",
+                monitor="val_loss",
                 min_delta=0.005,
-                patience=2,
+                patience=5,
                 restore_best_weights=True,
             ),
             StopOnValue(),
@@ -98,6 +114,10 @@ class MultiLabel(BinaryCrossMixin, BayesianOptimizationTunerMixin, CardModel):
         """
         batch_size = hp.Choice("batch_size", values=[16, 32, 64, 128, 256, 512])  # noqa
 
+        pl1 = self._pooling2d_choice("pooling1", hp)[1]
+        pl2 = self._pooling2d_choice("pooling2", hp)[1]
+        pl3 = self._pooling2d_choice("pooling3", hp)[1]
+
         m = models.Sequential(
             layers=[
                 layers.Conv2D(
@@ -107,30 +127,21 @@ class MultiLabel(BinaryCrossMixin, BayesianOptimizationTunerMixin, CardModel):
                     activation="relu",
                     input_shape=self.IMAGE_SHAPE,
                 ),
-                layers.MaxPooling2D(padding="same"),
+                pl1(padding="same"),
                 layers.Conv2D(64, (3, 3), padding="same", activation="relu"),
-                layers.MaxPool2D(padding="same"),
+                pl2(padding="same"),
                 layers.Conv2D(128, (3, 3), padding="same", activation="relu"),
-                layers.AveragePooling2D(padding="same"),
+                pl3(padding="same"),
                 layers.Conv2D(128, (3, 3), padding="same", activation="relu"),
-                layers.AveragePooling2D(padding="same"),
                 # layers.Dropout(0.2, seed=seed),
                 layers.Flatten(),
-                # layers.Dense(
-                #     hp.Int("dense_units", min_value=2**13, max_value=2**15, step=128),
-                #     activation="relu",
-                # ),
                 layers.Dense(
                     hp.Int(
                         "dense_units1",
-                        min_value=2**11,
+                        min_value=2**7,
                         max_value=2**13,
                         sampling="LOG",
                     ),
-                    activation="relu",
-                ),
-                layers.Dense(
-                    2944,
                     activation="relu",
                 ),
                 layers.Dense(len(self.labels), activation="sigmoid"),
@@ -138,26 +149,40 @@ class MultiLabel(BinaryCrossMixin, BayesianOptimizationTunerMixin, CardModel):
             name=self.name,
         )
 
-        optimizer = self._optimizer_choice(
-            "optimizer",
-            hp,
-            exclude={
-                "Adadelta",
-                "Adagrad",
-                "Adam",
-                "Amsgrad",
-                "Adamax",
-                "Ftrl",
-                "Nadam",
-            },
-        )[1](
-            momentum=hp.Float("mometum", min_value=0.0, max_value=0.999),
-            learning_rate=hp.Float(
-                "learning_rate", min_value=1.0e-7, max_value=1.0, sampling="LOG"
-            ),
-        )
+        optimizer = self._optimizer_choice("optimizer", hp)[1]
 
-        m.compile(optimizer=optimizer, loss=self.loss, metrics=self.metrics)
+        # optimizer = self._optimizer_choice(
+        #     "optimizer",
+        #     hp,
+        #     exclude={
+        #         "Adadelta",
+        #         "Adagrad",
+        #         "Adam",
+        #         "Amsgrad",
+        #         "Adamax",
+        #         "Ftrl",
+        #         "Nadam",
+        #     },
+        # )[1](
+        #     momentum=hp.Float("mometum", min_value=0.0, max_value=0.9),
+        #     learning_rate=hp.Float(
+        #         "learning_rate", min_value=1.0e-7, max_value=0.9, sampling="LOG"
+        #     ),
+        # )
+
+        m.compile(
+            optimizer=optimizer(),
+            loss=self.loss,
+            metrics=self._metrics
+            + [
+                MyOneHotMeanIoU(
+                    num_classes=len(self.labels), threshold=0.95, name="accuracy"
+                ),
+                # MyOneHotIoU(
+                #     num_classes=len(self.labels), target_class_ids=list(range(len(self.labels))), threshold=0.95, name="accuracy"
+                # ),
+            ],
+        )
         return m
 
 
