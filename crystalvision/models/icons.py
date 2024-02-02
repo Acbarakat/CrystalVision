7a9a43a4c4d1aee464728f18e275cfe3bc26fff3
Allonte <angelosheltie@gmail.com>
2024-02-02 14:09:47 -0800

    Update icons.py

--- crystalvision/models/icons.py
+++ crystalvision/models/icons.py
@@ -11,17 +11,21 @@ from keras import layers, models, optimizers
 from keras_tuner import HyperParameters
 
 try:
-    from . import CardModel, BinaryMixin, CategoricalMixin, \
-        BayesianOptimizationTunerMixin, RandomSearchTunerMixin
+    from . import (
+        CardModel,
+        BinaryMixin,
+    )
+    from .mixins.tuners import RandomSearchTunerMixin
 except ImportError:
-    from crystalvision.models import CardModel, BinaryMixin, \
-        CategoricalMixin, BayesianOptimizationTunerMixin, \
-        RandomSearchTunerMixin
+    from crystalvision.models import (
+        CardModel,
+        BinaryMixin,
+    )
+    from crystalvision.models.mixins.tuners import RandomSearchTunerMixin
+
 
 class Exburst(BinaryMixin, RandomSearchTunerMixin, CardModel):
-    def __init__(self,
-                 df: DataFrame,
-                 vdf: DataFrame) -> None:
+    def __init__(self, df: DataFrame, vdf: DataFrame) -> None:
         super().__init__(df, vdf, "ex_burst", name="exburst")
 
         self.stratify_cols.extend(["element", "type_en"])
@@ -47,9 +51,7 @@ class Exburst(BinaryMixin, RandomSearchTunerMixin, CardModel):
 
         return df
 
-    def build(self,
-              hp: HyperParameters,
-              seed: int | None = None) -> models.Sequential:
+    def build(self, hp: HyperParameters, seed: int | None = None) -> models.Sequential:
         """
         Build a model.
 
@@ -59,50 +61,54 @@ class Exburst(BinaryMixin, RandomSearchTunerMixin, CardModel):
         Returns:
             A model instance.
         """
-        m = models.Sequential(layers=[
-            layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=self.IMAGE_SHAPE),
-            layers.MaxPooling2D(),
-            layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Flatten(),
-            layers.Dense(hp.Int('dense_units', min_value=32, max_value=512, step=32), activation='relu'),
-            layers.Dense(1, activation="sigmoid")
-        ], name=self.name)
+        m = models.Sequential(
+            layers=[
+                layers.Conv2D(
+                    32,
+                    kernel_size=(3, 3),
+                    activation="relu",
+                    input_shape=self.IMAGE_SHAPE,
+                ),
+                layers.MaxPooling2D(),
+                layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Flatten(),
+                layers.Dense(
+                    hp.Int("dense_units", min_value=32, max_value=512, step=32),
+                    activation="relu",
+                ),
+                layers.Dense(1, activation="sigmoid"),
+            ],
+            name=self.name,
+        )
 
-        learning_rate = hp.Choice('learning_rate',
-                                  values=[0.01, 0.001, 0.0001])
-        optimizer = hp.Choice('optimizer',
-                              values=['adam', 'rmsprop', 'sgd'])
+        learning_rate = hp.Choice("learning_rate", values=[0.01, 0.001, 0.0001])
+        optimizer = hp.Choice("optimizer", values=["adam", "rmsprop", "sgd"])
         # learning_rate = hp.Float('learning_rate',
         #                          min_value=1e-4,
         #                          max_value=1e-2,
         #                          sampling='LOG')
 
-        if optimizer == 'adam':
+        if optimizer == "adam":
             optimizer = optimizers.Adam(learning_rate=learning_rate)
-        elif optimizer == 'rmsprop':
+        elif optimizer == "rmsprop":
             optimizer = optimizers.RMSprop(learning_rate=learning_rate)
         else:
             optimizer = optimizers.SGD(learning_rate=learning_rate)
 
-        m.compile(optimizer=optimizer,
-                  loss=self.loss,
-                  metrics=self.metrics)
+        m.compile(optimizer=optimizer, loss=self.loss, metrics=self.metrics)
         return m
 
 
 class Multicard(BinaryMixin, RandomSearchTunerMixin, CardModel):
-    def __init__(self,
-                 df: DataFrame,
-                 vdf: DataFrame) -> None:
+    def __init__(self, df: DataFrame, vdf: DataFrame) -> None:
         super().__init__(df, vdf, "multicard", name="multicard")
 
         self.stratify_cols.extend(["element", "type_en"])
-
 
     @staticmethod
     def filter_dataframe(df: DataFrame) -> DataFrame:
@@ -125,9 +131,7 @@ class Multicard(BinaryMixin, RandomSearchTunerMixin, CardModel):
 
         return df
 
-    def build(self,
-              hp: HyperParameters,
-              seed: int | None = None) -> models.Sequential:
+    def build(self, hp: HyperParameters, seed: int | None = None) -> models.Sequential:
         """
         Build a model.
 
@@ -137,39 +141,46 @@ class Multicard(BinaryMixin, RandomSearchTunerMixin, CardModel):
         Returns:
             A model instance.
         """
-        m = models.Sequential(layers=[
-            layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=self.IMAGE_SHAPE),
-            layers.MaxPooling2D(),
-            layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Flatten(),
-            layers.Dense(hp.Int('dense_units', min_value=32, max_value=512, step=32), activation='relu'),
-            layers.Dense(1, activation="sigmoid")
-        ], name=self.name)
+        m = models.Sequential(
+            layers=[
+                layers.Conv2D(
+                    32,
+                    kernel_size=(3, 3),
+                    activation="relu",
+                    input_shape=self.IMAGE_SHAPE,
+                ),
+                layers.MaxPooling2D(),
+                layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Flatten(),
+                layers.Dense(
+                    hp.Int("dense_units", min_value=32, max_value=512, step=32),
+                    activation="relu",
+                ),
+                layers.Dense(1, activation="sigmoid"),
+            ],
+            name=self.name,
+        )
 
-        learning_rate = hp.Choice('learning_rate',
-                                  values=[0.01, 0.001, 0.0001])
-        optimizer = hp.Choice('optimizer',
-                              values=['adam', 'rmsprop', 'sgd'])
+        learning_rate = hp.Choice("learning_rate", values=[0.01, 0.001, 0.0001])
+        optimizer = hp.Choice("optimizer", values=["adam", "rmsprop", "sgd"])
         # learning_rate = hp.Float('learning_rate',
         #                          min_value=1e-4,
         #                          max_value=1e-2,
         #                          sampling='LOG')
 
-        if optimizer == 'adam':
+        if optimizer == "adam":
             optimizer = optimizers.Adam(learning_rate=learning_rate)
-        elif optimizer == 'rmsprop':
+        elif optimizer == "rmsprop":
             optimizer = optimizers.RMSprop(learning_rate=learning_rate)
         else:
             optimizer = optimizers.SGD(learning_rate=learning_rate)
 
-        m.compile(optimizer=optimizer,
-                  loss=self.loss,
-                  metrics=self.metrics)
+        m.compile(optimizer=optimizer, loss=self.loss, metrics=self.metrics)
         return m
 
 
